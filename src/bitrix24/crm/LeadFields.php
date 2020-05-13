976064ff429eb82c5e0022b7c64a7573b02d8353
Лев Тарасов <l.tarasov@itcomgk.ru>
2020-05-13 15:26:23 +0300

    added lead fields;

--- src/bitrix24/crm/LeadFields.php
+++ src/bitrix24/crm/LeadFields.php
@@ -7,11 +7,12 @@ namespace itcomgk\bitrix24\crm;
  *
  * @package itcomgk\bitrix24\crm
  */
-class LeadFields
+class LeadFields extends CommonFields
 {
     const COMPANY_TITLE = 'COMPANY_TITLE';
     const FIRST_NAME = 'NAME';
     const LAST_NAME = 'LAST_NAME';
     const JOB_TITLE = 'POST';
+    const ADDRESS = 'ADDRESS';
     const ADDRESS_CITY = 'ADDRESS_CITY';
 }
---
+++ src/bitrix24/crm/LeadFieldsCustom.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace itcomgk\bitrix24\crm;
+
+/**
+ * Описание полей сущости Лида ITK
+ *
+ * @package itcomgk\bitrix24\crm
+ */
+class LeadFieldsCustom extends LeadFields
+{
+    const INN = 'UF_INN';
+    const OKVED_CODE = 'UF_OKVED_CODE';
+    const OKVED_TITLE = 'UF_OKVED_TITLE';
+}
